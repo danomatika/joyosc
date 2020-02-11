1caffc786ae4caceed8b3ac83f7c03b4e73db762
danomatika <danomatika@gmail.com>
2020-02-11 14:27:39 +0100

    update autogen for better mingw support

--- autogen.sh
+++ autogen.sh
@@ -2,9 +2,24 @@
 #
 # generate autotools files
 #
+PATH=/sw/bin:$PATH
+
+PWD=${0%/*}
 
 cd lib/lopack && ./autogen.sh && cd ../../
 cd lib/tinyobject && ./autogen.sh && cd ../../
 
-mkdir -p config m4
-autoreconf --install --force
+case `uname -s` in
+    MINGW*)
+        # autoreconf doesn't always work on MinGW
+        aclocal --force -I m4 && \
+        libtoolize --install --force && \
+        autoconf --force && \
+        automake --add-missing --copy --force-missing && \
+        true
+    ;;
+    *)
+        automake --add-missing
+        autoreconf --install --force --verbose
+    ;;
+esac
