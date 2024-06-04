7908fc18066b30412cb35e7fa5b7ccade0bac6b4
danomatika <danomatika@gmail.com>
2024-06-05 00:27:41 +0200

    autogen: try commands without chaining on windows

--- autogen.sh
+++ autogen.sh
@@ -8,11 +8,15 @@ set -x
 case `uname -s` in
     MINGW*)
         # autoreconf doesn't always work on MinGW
-        aclocal --force -I m4 && \
-        libtoolize --install --force && \
-        autoconf --force && \
-        automake --add-missing --copy --force-missing && \
-        true
+        # aclocal --force -I m4 && \
+        # libtoolize --install --force && \
+        # autoconf --force && \
+        # automake --add-missing --copy --force-missing && \
+        # true
+        aclocal --force -I m4
+        libtoolize --install --force
+        autoconf --force
+        automake --add-missing --copy --force-missing
     ;;
     *)
         aclocal --force -I m4
