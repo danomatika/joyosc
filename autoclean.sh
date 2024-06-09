4b2b67c3ea9134a4bae0442f59cb663d8a2d8a61
danomatika <danomatika@gmail.com>
2024-06-09 11:08:05 +0200

    no longer ignore premake or xcode files as premake support has been removed, remove *.in and ~ files whe autocleaning

--- autoclean.sh
+++ autoclean.sh
@@ -5,4 +5,5 @@
 
 rm -rf autom4te.cache config dist
 rm -f m4/libtool.m4 m4/lt*.m4
-rm -f INSTALL aclocal.m4 configure config.log config.status libtool
+rm -f INSTALL aclocal.m4 configure configure~ configure.ac~ config.log config.status libtool
+find . -name "*.in" -delete
