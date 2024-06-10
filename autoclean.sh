f54dec2551d45f2325399b682cee1c1abbd95f47
danomatika <danomatika@gmail.com>
2024-06-10 22:43:07 +0200

    remove header tmp ~ and src .deps when autocleaning

--- autoclean.sh
+++ autoclean.sh
@@ -7,3 +7,4 @@ rm -rf autom4te.cache config dist
 rm -f m4/libtool.m4 m4/lt*.m4
 rm -f INSTALL aclocal.m4 configure configure~ configure.ac~ config.log config.status libtool
 find . -name "*.in" -delete
+rm -rf src/config.h.in~ src/joyosc/.deps src/lsjs/.deps
