a001ada5b9af5bd1dfb3f5e7f4c319fd86ebfad6
danomatika <danomatika@gmail.com>
2024-11-13 21:58:47 +0100

    update header date attribution

--- src/joyosc/Event.h
+++ src/joyosc/Event.h
@@ -4,7 +4,7 @@
 
 	joyosc: a device event to osc bridge
   
-	Copyright (C) 2010 Dan Wilcox <danomatika@gmail.com>
+	Copyright (C) 2024 Dan Wilcox <danomatika@gmail.com>
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
